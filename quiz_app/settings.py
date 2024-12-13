8befe62389d3b21c0c214c990dcf03788467841c
pranav-85 <msaipranav2004@gmail.com>
2024-12-13 18:36:38 +0530

    deploy

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -127,4 +127,4 @@ STATIC_URL = 'quiz-react/public/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['https://quiz-react-b87zvztt4-pranavs-projects-3febebc8.vercel.app/']
