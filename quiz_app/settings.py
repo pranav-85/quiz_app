c611ea0bfdd042507fa429a60157905d6d886f4c
pranav <125111881+pranav-85@users.noreply.github.com>
2024-12-13 22:21:23 +0530

    Update settings.py

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware'
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
