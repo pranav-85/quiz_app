f311940fc4766de3df555a9c7c0306598f973cb6
pranav <125111881+pranav-85@users.noreply.github.com>
2024-12-13 22:13:18 +0530

    Update views.py

--- quiz/views.py
+++ quiz/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .models import Questions
 from .serializers import QuestionSerializer
 import random
 
 class QuestionListView(APIView):
+    permission_classes = [AllowAny]
     def get(self, request, format=None):
         question_ids = Questions.objects.values_list('id', flat=True)
         random_ids = random.sample(list(question_ids), 5)
