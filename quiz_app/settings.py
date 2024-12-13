f30aa24c54fd076002f562924b84871aae4eb520
pranav <125111881+pranav-85@users.noreply.github.com>
2024-12-13 21:42:44 +0530

    Update settings.py

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rv(&1odigx08%!81x1xe9_1-fej_l4afw)ap-am5eg@zo@&+pp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['quiz-app-d6xf.onrender.com', 'localhost', '127.0.0.1', 'quiz-react-b87zvztt4-pranavs-projects-3febebc8.vercel.app']
 
 
 # Application definition
